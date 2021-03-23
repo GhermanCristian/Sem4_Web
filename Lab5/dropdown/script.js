bda9ab0772a36fe8138ba3ab47bcb65ae837dd0b
GhermanCristian <gherman_cristi10@yahoo.com>
2021-03-24 00:46:31 +0200

    Lab 5 - Fixed bugs - Minor design changes

--- Lab5/dropdown/script.js
+++ Lab5/dropdown/script.js
@@ -1,40 +1,26 @@
 function showMenuByClass(menuClassName) {
 	var menuItems = document.getElementsByClassName(menuClassName);
-	for (i = 0; i < menuItems.length; i++) {
+	for (var i = 0; i < menuItems.length; i++) {
 		menuItems[i].style.display = "block";
 	}
 }
 
 function hideMenuByClass(menuClassName) {
 	var menuItems = document.getElementsByClassName(menuClassName);
-	for (i = 0; i < menuItems.length; i++) {
+	for (var i = 0; i < menuItems.length; i++) {
 		menuItems[i].style.display = "none";
-	}
-}
-
-function hideWithChildren(item) {
-	item.style.display = "none";
-	var children = item.childNodes;
-	for (i = 0; i < children.length; i++) {
-		if (children[i].nodeName == "BUTTON" || children[i].nodeName == "DIV") {
-			children[i].style.display = "none";
-		}
 	}
 }
 
 window.onclick = function(event) {
 	if (!event.target.closest('#mainMenuButton') && !event.target.closest('#myDropdown')) {
-		var dropdowns = document.getElementsByClassName("submenu");
-		for (var i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			hideWithChildren(openDropdown);
-		}
+		hideMenuByClass("submenu");
 	}
-	
+
 	var baseClassName = ".submenu";
 	var baseButtonName = "#submenuButton";
 	var baseName = "submenu";
-	for (i = 1; i <= 5; i++) {
+	for (var i = 1; i <= 5; i++) {
 		if (!event.target.closest(baseClassName + i) && !event.target.closest(baseButtonName + i)) {
 			hideMenuByClass(baseName + i);
 		}
