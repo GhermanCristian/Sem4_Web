58cb2fd916bbf24cd8a966f5178e32c39fcda2da
GhermanCristian <gherman_cristi10@yahoo.com>
2021-03-27 14:53:37 +0200

    Lab 6 - Added border selection

--- Lab6/buttonDialogWindow/script.js
+++ Lab6/buttonDialogWindow/script.js
@@ -14,10 +14,23 @@ $(document).ready(function() {
 		let leftMargin = dialogBoxData.x;
 		let topMargin = dialogBoxData.y;
 		let borderWidth = parseInt(dialogBox.css('border-top-width')); // for now I'll assume that all borders have the same width
-		let width = dialogBox.width; // dialogBoxData.width = 2 * border + inner width
-		let height = dialogBox.height; // same but for heights*/
+		let width = dialogBoxData.width; // dialogBoxData.width = 2 * border + inner width
+		let height = dialogBoxData.height; // same but for heights*/
 		
+		let x = e.clientX - leftMargin;
+		let y = e.clientY - topMargin;
+
 		// upper and lower margins => x - leftMargin in [0; width], y - topMargin in [0; borderWidth] U [height - borderWidth; height];
 		// left and right margins => x - leftMargin in [0; borderWidth] U [width - borderWidth; width], y - topMargin in [0; height]
+		if (((0 <= x && x <= width) && ((0 <= y && y <= borderWidth) || (height - borderWidth <= y && y <= height))) || 
+			(((0 <= x && x <= borderWidth) || (width - borderWidth <= x && x <= width)) && (0 <= y && y <= height))){
+			var myButton = document.getElementById("myButton")
+			myButton.style.backgroundColor = "red";
+		}
+		else {
+			var myButton = document.getElementById("myButton")
+			myButton.style.backgroundColor = "black";
+		}
+		
 	});
 })
