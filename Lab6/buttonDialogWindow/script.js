50ceed091cee31eb3004739e0df0d4d17235a9d4
GhermanCristian <gherman_cristi10@yahoo.com>
2021-03-30 09:39:12 +0300

    Lab 6 - Increased the minimum border size - Minor improvements

--- Lab6/buttonDialogWindow/script.js
+++ Lab6/buttonDialogWindow/script.js
@@ -32,26 +32,26 @@ $(document).ready(function() {
 
 	$(this).mousemove(function(e) {
 		let dialogBox = document.getElementById("dialog");
-		let dialogBoxData = dialogBox.getBoundingClientRect();
 		if (dialogBox.style.display === "" || dialogBox.style.display === "none") {
 			return; // don't care about the dialog box if it hasn't been displayed yet
 		}
+		let dialogBoxData = dialogBox.getBoundingClientRect();
 		
 		dialogBox = $("#dialog");
 		let leftMargin = dialogBoxData.x;
 		let topMargin = dialogBoxData.y;
 		let borderWidth = parseInt(dialogBox.css('border-top-width')); // for now I'll assume that all borders have the same width
-		if (borderWidth < 5) {
-			borderWidth = 5; // if the border is too small, it's almost imposible to click on it
+		if (borderWidth < 10) {
+			borderWidth = 10; // if the border is too small, it's almost imposible to click on it
 		}
 		let width = dialogBoxData.width; // dialogBoxData.width = 2 * border + inner width
-		let height = dialogBoxData.height; // same but for heights*/
+		let height = dialogBoxData.height; // same but for heights
 		
 		let x = e.clientX - leftMargin;
 		let y = e.clientY - topMargin;
 
-		// upper and lower margins => x - leftMargin in [0; width], y - topMargin in [0; borderWidth] U [height - borderWidth; height];
-		// left and right margins => x - leftMargin in [0; borderWidth] U [width - borderWidth; width], y - topMargin in [0; height]
+		// upper and lower borders => x - leftMargin in [0; width], y - topMargin in [0; borderWidth] U [height - borderWidth; height];
+		// left and right borders => x - leftMargin in [0; borderWidth] U [width - borderWidth; width], y - topMargin in [0; height]
 		if (((0 <= x && x <= width) && ((0 <= y && y <= borderWidth) || (height - borderWidth <= y && y <= height))) || 
 			(((0 <= x && x <= borderWidth) || (width - borderWidth <= x && x <= width)) && (0 <= y && y <= height))){
 			doResize = 1;
