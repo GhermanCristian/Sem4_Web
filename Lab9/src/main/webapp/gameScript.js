dec8f96c64d467407f6ee24cfc2549e75a9ff0da
GhermanCristian <gherman_cristi10@yahoo.com>
2021-05-08 21:53:02 +0300

    Lab 9 - First working version of the game

--- Lab9/src/main/webapp/gameScript.js
+++ Lab9/src/main/webapp/gameScript.js
@@ -1,42 +1,81 @@
 const BOARD_SIZE = 20; // no. of tiles
 const TILE_SIZE = 20;
-const DIRECTIONS_X = [0, 1, 0, -1]; // up, left, down, right
-const DIRECTIONS_Y = [-1, 0, 1, 0];
+const DIRECTIONS_X = [-1, 0, 1, 0]; // left, up, right, down
+const DIRECTIONS_Y = [0, -1, 0, 1];
 
-function drawSnakePart(snakePart, boardContext) {
-    boardContext.fillStyle = 'lightblue';
-    boardContext.strokeStyle = 'darkblue';
-    boardContext.fillRect(snakePart.x * TILE_SIZE, snakePart.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-    boardContext.strokeRect(snakePart.x * TILE_SIZE, snakePart.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-}
+const LEFT_ARROW_CODE = 37;
+const UP_ARROW_CODE = 38;
+const RIGHT_ARROW_CODE = 39;
+const DOWN_ARROW_CODE = 40;
+const ESC_CODE = 27;
 
-function clearBoard(boardContext) {
-    boardContext.fillStyle = 'lightblue';
-    boardContext.strokeStyle = 'darkblue';
-    boardContext.fillRect(0, 0, 400, 400);
-    boardContext.strokeRect(0, 0, 400, 400);
-}
+class GameEngine {
+    constructor() {
+        this.pressedKeyCode = -1;
+        this.running = true;
+        this.snake = [{x: 1, y: 3}, {x: 1, y: 2}, {x: 1, y: 1}, {x: 1, y: 0}, ];
+        this.boardCanvas = document.getElementById("boardCanvas");
+        this.boardContext = this.boardCanvas.getContext("2d");
 
-function drawSnake(snake, boardContext) {
-    clearBoard(boardContext);
-    snake.forEach(snakePart => drawSnakePart(snakePart, boardContext));
-}
+        const gameEngineObj = this;
+        $(document).keydown(function(event) {
+            return gameEngineObj.pressKey(event);
+        });
+    }
 
-function moveSnakeOneStep(snake, boardContext, directionIndex) {
-    let newHead = {x: snake[0].x + DIRECTIONS_X[directionIndex], y: snake[0].y + DIRECTIONS_Y[directionIndex]};
-    snake.unshift(newHead);
-    snake.pop();
-    // send request to server with the move
+    drawSnakePart(snakePart) {
+        this.boardContext.fillStyle = 'lightblue';
+        this.boardContext.strokeStyle = 'darkblue';
+        this.boardContext.fillRect(snakePart.x * TILE_SIZE, snakePart.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        this.boardContext.strokeRect(snakePart.x * TILE_SIZE, snakePart.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+    clearBoard() {
+        this.boardContext.fillStyle = 'lightblue';
+        this.boardContext.strokeStyle = 'darkblue';
+        this.boardContext.fillRect(0, 0, 400, 400);
+        this.boardContext.strokeRect(0, 0, 400, 400);
+    }
+
+    drawSnake() {
+        this.clearBoard();
+        this.snake.forEach(snakePart => this.drawSnakePart(snakePart));
+    }
+    
+    moveSnakeOneStep() {
+        if (this.pressedKeyCode === -1) {
+            return;
+        }
+
+        let newHead = {x: this.snake[0].x + DIRECTIONS_X[this.pressedKeyCode], y: this.snake[0].y + DIRECTIONS_Y[this.pressedKeyCode]};
+        this.snake.unshift(newHead);
+        this.snake.pop();
+        // send request to server with the move
+    }
+
+    pressKey(event) {
+        let validKeys = [LEFT_ARROW_CODE, UP_ARROW_CODE, RIGHT_ARROW_CODE, DOWN_ARROW_CODE];
+        if (validKeys.includes(event.which)) {
+            this.pressedKeyCode = event.which - LEFT_ARROW_CODE;
+        }
+        else if (event.which === ESC_CODE) {
+            this.running = false;
+        }
+    }
+
+    gameLoop() {
+        let gameEngineObject = this;
+        setTimeout(function() {
+            if (gameEngineObject.running === true) {
+                gameEngineObject.drawSnake();
+                gameEngineObject.moveSnakeOneStep();
+                gameEngineObject.gameLoop();
+            }
+        }, 500);
+    }
 }
 
 $(document).ready(function() {
-    const boardCanvas = document.getElementById("boardCanvas");
-    const boardContext = boardCanvas.getContext("2d");
-    let snake = [{x: 1, y: 3}, {x: 1, y: 2}, {x: 1, y: 1}, {x: 1, y: 0}, ];
-
-    drawSnake(snake, boardContext);
-    moveSnakeOneStep(snake, boardContext, 1);
-    moveSnakeOneStep(snake, boardContext, 2);
-    moveSnakeOneStep(snake, boardContext, 1);
-    drawSnake(snake, boardContext);
+    const gameEngine = new GameEngine();
+    gameEngine.gameLoop();
 });
