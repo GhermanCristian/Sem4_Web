dc2defb48f6098b3a5fed9591283ce378b7b9556
GhermanCristian <gherman_cristi10@yahoo.com>
2021-05-08 14:27:35 +0300

    Lab 9 - It now redirects to specific pages if the login is successful or not - Some refactoring

--- Lab9/src/main/java/ro/ubb/Lab9/LoginServlet.java
+++ Lab9/src/main/java/ro/ubb/Lab9/LoginServlet.java
@@ -1,34 +1,38 @@
 package ro.ubb.Lab9;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import ro.ubb.Lab9.DBConnection;
-
 @WebServlet(name = "loginServlet", value = "/loginServlet")
 public class LoginServlet extends HttpServlet {
+    private int getCount(String username, String password, Connection connection) throws SQLException {
+        PreparedStatement checkCredentials = connection.prepareStatement("SELECT COUNT(*) FROM user WHERE username = ? AND password = ?");
+        checkCredentials.setString(1, username);
+        checkCredentials.setString(2, password);
+        ResultSet result = checkCredentials.executeQuery();
+        result.next();
+        int count = result.getInt(1);
+        result.close();
+        checkCredentials.close();
+        return count;
+    }
+
     private boolean areCredentialsValid(String username, String password) {
         boolean areValid = false;
-        try {
-            Connection connection = DBConnection.initializeDB();
-            PreparedStatement checkCredentials = connection.prepareStatement("SELECT COUNT(*) FROM user WHERE username = ? AND password = ?");
-            checkCredentials.setString(1, username);
-            checkCredentials.setString(2, password);
-            ResultSet result = checkCredentials.executeQuery();
-            result.next();
-            if (result.getInt(1) == 1) {
-                areValid = true;
+
+        try(Connection connection = DBConnection.initializeDB()) {
+            if (this.getCount(username, password, connection) == 1) {
+                areValid = true; // don't return here bc I want to close the connection
             }
-            checkCredentials.close();
-            connection.close();
         }
         catch (SQLException | ClassNotFoundException throwables) {
             throwables.printStackTrace();
@@ -37,15 +41,15 @@ public class LoginServlet extends HttpServlet {
         return areValid;
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-        String responseText = "not valid";
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
+        RequestDispatcher redirect = null;
         if (this.areCredentialsValid(request.getParameter("username"), request.getParameter("password"))) {
-            responseText = request.getParameter("username") + " -> " + request.getParameter("password");
+            redirect = request.getRequestDispatcher("/game.jsp");
         }
-        out.println("<h1>" + responseText + "</h1>");
-        out.println("</body></html>");
+        else {
+            redirect = request.getRequestDispatcher("/loginError.jsp");
+        }
+        redirect.forward(request, response);
     }
 }
