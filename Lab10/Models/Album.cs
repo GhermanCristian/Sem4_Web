078d068957f87e6c0e02d9d98bf87863ad18f3d6
GhermanCristian <gherman_cristi10@yahoo.com>
2021-05-16 16:47:44 +0300

    Lab 10 - Added the Album entity

---
+++ Lab10/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab10.Models {
+    public class Album {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public int Sales { get; set; }
+    }
+}
