434a154db65952dadfb95b4af4ba8bebc85ba97a
GhermanCristian <gherman_cristi10@yahoo.com>
2021-05-16 16:35:01 +0300

    Lab 10 - Initial commit

---
+++ Lab10/Data/MyDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+using Lab10.Models;
+
+namespace Lab10.Data {
+    public class MyDBContext : DbContext {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) {
+
+        }
+    }
+}
